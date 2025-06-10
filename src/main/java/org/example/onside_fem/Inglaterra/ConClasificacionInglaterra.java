5025ac9277c41c18cefdf3666e3b8a68765db58b
AliciaPM08 <apachecomena@gmail.com>
2025-06-10 19:27:32 +0200

    Idiomas y ayuda Clasificacion

--- src/main/java/org/example/onside_fem/Inglaterra/ConClasificacionInglaterra.java
+++ src/main/java/org/example/onside_fem/Inglaterra/ConClasificacionInglaterra.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.*;
@@ -18,9 +19,7 @@ import org.example.onside_fem.BBDD.Clasificacion;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class ConClasificacionInglaterra {
     @FXML
@@ -32,6 +31,8 @@ public class ConClasificacionInglaterra {
     @FXML
     private Menu menuSelecciones;
 
+    @FXML private Menu menuInicio;
+
     @FXML
     private ComboBox<String> comboBoxIdiomas;
 
@@ -40,6 +41,24 @@ public class ConClasificacionInglaterra {
 
     @FXML
     private Button btnEquipo;
+
+    @FXML private MenuItem menuAlemania;
+    @FXML private MenuItem menuAustralia;
+    @FXML private MenuItem menuBrasil;
+    @FXML private MenuItem menuCanada;
+    @FXML private MenuItem menuColombia;
+    @FXML private MenuItem menuEspana;
+    @FXML private MenuItem menuEstados;
+    @FXML private MenuItem menuFrancia;
+    @FXML private MenuItem menuInglaterra;
+    @FXML private MenuItem menuNigeria;
+    @FXML private MenuItem menuNZelanda;
+    @FXML private MenuItem menuSudafrica;
+    @FXML private MenuItem menuSuecia;
+
+    @FXML private Label footer;
+
+    @FXML private Label labelCampeonas, labelDescenso, labelChampions;
 
 
     private final Map<String, String> ligaPantallas = new HashMap<>();
@@ -58,11 +77,17 @@ public class ConClasificacionInglaterra {
     @FXML private TableColumn<Clasificacion, Integer> colFavor;
     @FXML private TableColumn<Clasificacion, Integer> colContra;
 
-    private String ligaActual = "Arkema Première Ligue";
+    private String ligaActual = "Womens Super League";
+
+    private ResourceBundle recursos;
+    private Locale localeActual = new Locale("es", "ES");
 
     @FXML
     public void initialize() {
         inicializarIdioma();
+        recursos = ResourceBundle.getBundle("idiomas.messages", localeActual);
+        traducirUI();
+
         inicializarRutas();
         inicializarMenuInicio();
         inicializarLigas();
@@ -73,15 +98,70 @@ public class ConClasificacionInglaterra {
     }
 
     private void inicializarIdioma() {
-        comboBoxIdiomas.getItems().addAll("Español", "Inglés");
+        comboBoxIdiomas.getItems().addAll("Español", "English");
         comboBoxIdiomas.setValue("Español");
         comboBoxIdiomas.setOnAction(e -> cambiarIdioma());
     }
 
     private void cambiarIdioma() {
-        String idioma = comboBoxIdiomas.getValue();
-        Locale locale = idioma.equals("Inglés") ? new Locale("en", "US") : new Locale("es", "ES");
-        System.out.println("Idioma cambiado a: " + idioma);
+        String idiomaSeleccionado = comboBoxIdiomas.getValue();
+        if (idiomaSeleccionado.equals("English")) {
+            localeActual = new Locale("en", "US");
+        } else {
+            localeActual = new Locale("es", "ES");
+        }
+
+        try {
+            recursos = ResourceBundle.getBundle("idiomas.messages", localeActual);
+            traducirUI();
+        } catch (MissingResourceException e) {
+            System.err.println("Archivo de idioma no encontrado.");
+        }
+    }
+
+    private void traducirUI() {
+        //Menu
+        menuInicio.setText(recursos.getString("menu.inicio"));
+        menuLigas.setText(recursos.getString("menu.ligas"));
+        menuSelecciones.setText(recursos.getString("menu.selecciones"));
+        menuAlemania.setText(recursos.getString("menu.alemania"));
+        menuAustralia.setText(recursos.getString("menu.australia"));
+        menuBrasil.setText(recursos.getString("menu.brasil"));
+        menuCanada.setText(recursos.getString("menu.canada"));
+        menuColombia.setText(recursos.getString("menu.colombia"));
+        menuEspana.setText(recursos.getString("menu.espana"));
+        menuEstados.setText(recursos.getString("menu.unidos"));
+        menuFrancia.setText(recursos.getString("menu.francia"));
+        menuInglaterra.setText(recursos.getString("menu.inglaterra"));
+        menuNigeria.setText(recursos.getString("menu.nigeria"));
+        menuNZelanda.setText(recursos.getString("menu.nzelanda"));
+        menuSudafrica.setText(recursos.getString("menu.sudafrica"));
+        menuSuecia.setText(recursos.getString("menu.suecia"));
+        volverItem.setText(recursos.getString("menu.volver"));
+        hyperlinkAyuda.setText(recursos.getString("menu.ayuda"));
+
+        //Boton
+        btnEquipo.setText(recursos.getString("btn.equipo"));
+
+
+        //Footer
+        footer.setText(recursos.getString("footer.copy"));
+
+        //Columnas
+        colNombre.setText(recursos.getString("clasificacion.colNombre"));
+        colPosicion.setText(recursos.getString("clasificacion.colPosicion"));
+        colPuntos.setText(recursos.getString("clasificacion.colPuntos"));
+        colJugados.setText(recursos.getString("clasificacion.colJugados"));
+        colVictorias.setText(recursos.getString("clasificacion.colVictorias"));
+        colEmpates.setText(recursos.getString("clasifcacion.colEmpates"));
+        colPerdidos.setText(recursos.getString("clasificacion.colPerdidos"));
+        colFavor.setText(recursos.getString("clasificacion.colFavor"));
+        colContra.setText(recursos.getString("clasificacion.colContra"));
+
+        labelCampeonas.setText(recursos.getString("labelCampeonas.c"));
+        labelDescenso.setText(recursos.getString("labelDescenso.d"));
+        labelChampions.setText(recursos.getString("labelChampions.ch"));
+
     }
 
     private void inicializarMenuInicio() {
@@ -92,25 +172,33 @@ public class ConClasificacionInglaterra {
         // LIGAS
         ligaPantallas.put("Finetwork Liga F", "/org/example/onside_fem/Espana/PPLigaEspanola.fxml");
         ligaPantallas.put("Liberty A-League", "/org/example/onside_fem/Australia/PPLigaAustraliana.fxml");
-        ligaPantallas.put("Women's Super League", "/org/example/onside_fem/FXML/Ligas/WomensSuperLeague.fxml");
+        ligaPantallas.put("Women's Super League", "/org/example/onside_fem/Inglaterra/PPLigaInglaterra.fxml");
         ligaPantallas.put("Arkema Premiere League", "/org/example/onside_fem/Francia/PPLigaFrancia.fxml");
         ligaPantallas.put("Yogibo WE League", "/org/example/onside_fem/Japon/PPLigaJaponesa.fxml");
         ligaPantallas.put("National Super League", "/org/example/onside_fem/EEUU/PPLigaEEUU.fxml");
 
         // SELECCIONES
         seleccionPantallas.put("Alemania", "/org/example/onside_fem/Selecciones/PAlemania.fxml");
+        seleccionPantallas.put("Germany", "/org/example/onside_fem/Selecciones/PAlemania.fxml");
         seleccionPantallas.put("Australia", "/org/example/onside_fem/Selecciones/PAustralia.fxml");
         seleccionPantallas.put("Brasil", "/org/example/onside_fem/Selecciones/PBrasil.fxml");
+        seleccionPantallas.put("Brazil", "/org/example/onside_fem/Selecciones/PBrasil.fxml");
         seleccionPantallas.put("Canada", "/org/example/onside_fem/Selecciones/PCanada.fxml");
         seleccionPantallas.put("Colombia", "/org/example/onside_fem/Selecciones/PColombia.fxml");
         seleccionPantallas.put("España", "/org/example/onside_fem/Selecciones/PEspana.fxml");
+        seleccionPantallas.put("Spain", "/org/example/onside_fem/Selecciones/PEspana.fxml");
         seleccionPantallas.put("Estados Unidos", "/org/example/onside_fem/Selecciones/PEEUU.fxml");
+        seleccionPantallas.put("USA", "/org/example/onside_fem/Selecciones/PEEUU.fxml");
         seleccionPantallas.put("Francia", "/org/example/onside_fem/Selecciones/PFrancia.fxml");
+        seleccionPantallas.put("France", "/org/example/onside_fem/Selecciones/PFrancia.fxml");
         seleccionPantallas.put("Inglaterra", "/org/example/onside_fem/Selecciones/PInglaterra.fxml");
+        seleccionPantallas.put("England", "/org/example/onside_fem/Selecciones/PInglaterra.fxml");
         seleccionPantallas.put("Nigeria", "/org/example/onside_fem/Selecciones/PNigeria.fxml");
         seleccionPantallas.put("Nueva Zelanda", "/org/example/onside_fem/Selecciones/PNuevaZelanda.fxml");
+        seleccionPantallas.put("New Zeland", "/org/example/onside_fem/Selecciones/PNuevaZelanda.fxml");
         seleccionPantallas.put("Sudafrica", "/org/example/onside_fem/Selecciones/PSudafrica.fxml");
         seleccionPantallas.put("Suecia", "/org/example/onside_fem/Selecciones/PSuecia.fxml");
+        seleccionPantallas.put("Sweden", "/org/example/onside_fem/Selecciones/PSuecia.fxml");
     }
 
     private void inicializarLigas() {
